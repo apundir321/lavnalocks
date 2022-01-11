06621adb65a4c49d8c120f96f8af79a7f8ab45eb
Anurag Pundir <anuragpundir621@gmail.com>
2022-01-11 09:38:13 +0530

    updatin prices

--- backend/src/seedDB/category-seed.js
+++ backend/src/seedDB/category-seed.js
@@ -24,44 +24,44 @@ async function seedDB() {
     await mongoose.disconnect();
   }
   await seedCateg("L-A8",14200,"/assets/img/lavna3.jpg",9900,"Meet the 4 in 1 Digital gate lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, and Key.",null,"feature-LA8");
-  await seedCateg("L-A24-BLACK",14200,"/assets/img/lavna4.jpg",8300,"Meet the 6 in 1 Digital fingerprint lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",[
-    {
-        "title": "L-A24(Bluetooth)",
-        "price": 14200,
-        "sellingPrice": 8300,
-        "image": "assets/img/lavna11.jpg"
-    },
-    {
-        "title": "L-A24(WI-FI)",
-        "price": 15700,
-        "sellingPrice": 9800,
-        "image": "assets/img/lavna11.jpg"
-    }
-],"features-L-A24-BLACK");
-  await seedCateg("L-A24-GOLD",14200,"/assets/img/lavna4.jpg",8300,"Meet the 6 in 1 Digital fingerprint lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",[
-    {
-        "title": "L-A24(Bluetooth)",
-        "price": 14200,
-        "sellingPrice": 8300,
-        "image": "/assets/img/lavna4.jpg"
-    },
-    {
-        "title": "L-A24(WI-FI)",
-        "price": 15700,
-        "sellingPrice": 9800,
-        "image": "/assets/img/lavna4.jpg"
-    }
-],"features-L-A24-GOLD");
+//   await seedCateg("L-A24-BLACK",14200,"/assets/img/lavna4.jpg",8300,"Meet the 6 in 1 Digital fingerprint lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",[
+//     {
+//         "title": "L-A24(Bluetooth)",
+//         "price": 14200,
+//         "sellingPrice": 8300,
+//         "image": "assets/img/lavna11.jpg"
+//     },
+//     {
+//         "title": "L-A24(WI-FI)",
+//         "price": 15700,
+//         "sellingPrice": 9800,
+//         "image": "assets/img/lavna11.jpg"
+//     }
+// ],"features-L-A24-BLACK");
+//   await seedCateg("L-A24-GOLD",14200,"/assets/img/lavna4.jpg",8300,"Meet the 6 in 1 Digital fingerprint lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",[
+//     {
+//         "title": "L-A24(Bluetooth)",
+//         "price": 14200,
+//         "sellingPrice": 8300,
+//         "image": "/assets/img/lavna4.jpg"
+//     },
+//     {
+//         "title": "L-A24(WI-FI)",
+//         "price": 15700,
+//         "sellingPrice": 9800,
+//         "image": "/assets/img/lavna4.jpg"
+//     }
+// ],"features-L-A24-GOLD");
   await seedCateg("L-A28-BLACK",8999,"/assets/img/lavna5.jpg",7600,"Meet the 6 in 1 Fingerprint door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",null,"features-L-A28-BLACK");
-  await seedCateg("L-A28-GOLD",8999,"/assets/img/lavna6.jpg",7600,"Meet the 6 in 1 Fingerprint door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",null,"features-L-A28-GOLD");
+  await seedCateg("L-A28-GOLD",8999,"/assets/img/lavna6.jpg",7799,"Meet the 6 in 1 Fingerprint door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",null,"features-L-A28-GOLD");
   await seedCateg("L-A15",4999,"/assets/img/LA15/1.jpg",3899,"Meet the 2 in 1 MOST Affordable Digital door lock from LAVNA Locks, which you can access from your fingerprint and Key.",null,"features-L-A15");
   await seedCateg("L-H300",7500,"/assets/img/LH300/2.jpg",3850,"Meet the 2 in 1 smart lock for hotel doors from LAVNA Locks, which you can access from your RFID Card and Key.",null,"features-L-H300");
   await seedCateg("L-A5",13900,"/assets/img/LA5/1.jpg",8599,"Meet the 4 in 1 Glass door security lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN and remote",null,"features-L-A5");
   await seedCateg("L-S9",5900,"/assets/img/ls9/1.jpg",3180,"Meet the 2 in 1 Lock for your cabinets from LAVNA Locks, which you can access from your fingerprint and PIN.",null,"features-L-S9");
   // await seedCateg("L-A24(Bluetooth)",14200,"/assets/img/lavna4.jpg",8799,"Meet the 2 in 1 Lock for your doors from LAVNA Locks, which you can access from your fingerprint and PIN.",null,"features-L-A24(Bluetooth)");
   await seedCateg("L-A24(WI-FI)",15700,"/assets/img/la24/2.jpg",9499,"Meet the 5 in 1 Digital door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, WI-FI unlocking and manual key.",null,"features-L-A24(WI-FI)");
-  await seedCateg("L-A24-GOLD",14200,"/assets/img/lavna6.jpg",7600,"Meet the 6 in 1 Fingerprint door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",null,"features-L-A24-GOLD");
-  await seedCateg("L-A24-Black(Bluetooth)",14200,"/assets/img/la24/1_bluetooth.jpg",9199,"Meet the 6 in 1 Digital door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",null,"features-L-A24-Black(Bluetooth)");
+  await seedCateg("L-A24-GOLD",14200,"/assets/img/lavna6.jpg",9499,"Meet the 6 in 1 Fingerprint door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",null,"features-L-A24-GOLD");
+  await seedCateg("L-A24-Black(Bluetooth)",14200,"/assets/img/la24/1_bluetooth.jpg",9499,"Meet the 6 in 1 Digital door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, Bluetooth unlocking, OTP Unlocking and Key.",null,"features-L-A24-Black(Bluetooth)");
   // await seedCateg("L-A24-Black(WI-FI)",15700,"/assets/img/la24/3.jpg",8799,"Meet the 5 in 1 Digital door lock from LAVNA Locks, which you can access from your fingerprint, RFID Card, PIN, WI-FI unlocking.",null,"features-L-A24-Black(WI-FI)");
   await seedCateg("L-A28(Bluetooth)",8999,"/assets/img/lavna6.jpg",7699,"Meet the 6 in 1 Lock for your doors from LAVNA Locks, which you can access from your Fingerprint, PIN, RFID card, OTP unlocking, Bluetooth unlocking and Manual Key.",null,"features-L-A28(Bluetooth)");
   await seedCateg("L-A28(WI-FI)",9999,"/assets/img/LA28_gold/9.jpg",8699,"Meet the 6 in 1 Lock for your doors from LAVNA Locks, which you can access from your Fingerprint, PIN, RFID card, OTP unlocking, WI-FI unlocking and Manual Key.",null,"features-L-A28(WI-FI)");
@@ -71,7 +71,7 @@ async function seedDB() {
   await seedCateg("L-A28-Black(Remote)",9999,"/assets/img/LA28_black/8.jpg",9000,"Meet the 5 in 1 Lock for your doors from LAVNA Locks, which you can access from your Fingerprint, PIN, RFID card, Remote and Manual Key . ",null,"features-L-A28-Black(Remote)");
   
   await seedCateg("L-H300(Encoder)",7500,"/assets/img/LA28_black/8.jpg",3800,"Meet the 2 in 1 smart lock for hotel doors from LAVNA Locks, which you can access from your RFID Card and Key.",null,"features-L-H300(Encoder)");
-  await seedCateg("L-E50",7500,"/assets/img/LE50/1.jpg",6999,"Meet the 2 in 1 smart lock for hotel doors from LAVNA Locks, which you can access from your RFID Card and Key.",null,"features-L-E50");
+  await seedCateg("L-E50",9900,"/assets/img/LE50/1.jpg",6999,"Meet the 2 in 1 smart lock for hotel doors from LAVNA Locks, which you can access from your RFID Card and Key.",null,"features-L-E50");
 
   
   await closeDB();
